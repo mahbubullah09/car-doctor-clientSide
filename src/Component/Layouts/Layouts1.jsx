17673b614faf53864b3fb33136b5b8309bb5de56
Mahbubullah <mahbubullahpathan@gmail.com>
2023-10-29 00:00:38 +0600

    footer

--- src/Component/Layouts/Layouts1.jsx
+++ src/Component/Layouts/Layouts1.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 
 const Layouts1 = () => {
     return (
         <div>
             <Navbar/>
+            <Outlet/>
+            <Footer/>
             
         </div>
     );
